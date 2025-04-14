af2155d018bf9057109392644678148cf92b6093
Trevor <apple@Trevors-MacBook-Pro.local>
2025-04-14 19:54:03 +0100

    Update controls.html and refactor song layout

---
+++ combine-songs.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const songsDir = path.join(__dirname, 'songs');
+const outputFile = path.join(__dirname, 'songs-data.json');
+
+const songFiles = fs.readdirSync(songsDir).filter(file => file.endsWith('.js') && file !== 'combine-songs.js' );
+
+const songs = songFiles.map(file => {
+  const songPath = path.join(songsDir, file);
+  return require(songPath); // Imports the song object
+});
+
+fs.writeFileSync(outputFile, JSON.stringify(songs, null, 2), 'utf-8');
+console.log(`✅ Combined ${songs.length} songs into songs-data.json`);
+
+// node combine-songs.js
---
+++ songs/主祢知道.js
@@ -0,0 +1,69 @@
+module.exports = {
+  title: "主祢知道",
+  baseKey: "F#",
+  lyrics: `
+  Intro
+  | D#m7 - - A#m/C#  | Badd9 | F#maj9 | Badd9 - Bm6 - |
+  Verse 1
+    F#  C#/F  D#m7     
+  軟弱 主祢知道         
+   Bmaj7   A#m7      D#m7
+   疲乏    主已知道
+    G#m7       C#7        A#m7    D#7(b9)
+  更知道 我  渴慕  我心裡 那  愛慕
+  G#m7      Bm6    F#    B/C#
+  這一切 祢  都知道
+  
+  Chorus 1
+     F#add9      C#/F       D#m7   D#m7/C#
+  耶穌 親      自尋找    我
+     Bmaj7   C#/B      A#m7      D#7(b9)
+  於加利利海    邊 問我的心如何     
+     Cm7(b5)    Bdim7   A#m7      D#m7
+  縱使我曾軟    弱 祢  卻沒有捨  棄
+     G#m7      B/C#           D#m7
+  願一生跟主   奉上給  祢
+  
+  Music Break 1
+  | D#m7 - - - | Bmaj7 - - - | F#maj7 - - - | Dmaj9 - B/C# - |
+  Music Break 2
+  | D#m7 - - - | Bmaj7 - - - | F#maj7 - - - | B/C# - - - |
+  Verse 2
+     F#      C#/F       D#m7
+  軟弱 主會     擁抱      
+   Bmaj7   A#m7      D#m7
+  疲   乏 主    也鼓     舞
+    G#m7       C#7        A#m7    D#7(b9)
+  祢安慰 祢  引導 祢寬恕 祢  教導
+  G#m7     B/C#  D#m7  C#/F
+  這一切 我  都知道
+  
+  Bridge
+  Bmaj7    C#/B   A#m7    D#m7
+  我不回      首          向前     走
+    G#m7   B/C#          F#     C#/F#
+  點起愛祢的火     永不放手
+  Bmaj7   C#/B    A#m7       D#m7 
+  祢應許不改變         必施恩看顧
+        G#m7   B/C#     D#m7  C#/F
+  賜我勇氣走上   新的方       向
+  
+  Chorus 2 (升key)
+     Abadd9   Eb/G     Fm7   Fm7/Eb
+  耶穌       親自尋找    我
+  Dbmaj7   Eb/Db       Abmaj9   F7(b9)  Dm7(b5)
+  漆黑迷途之 中 讓我再 見真光
+  Dbmaj7    Dbdim7   Cm7      Fm7   F/A
+  縱使我曾軟     弱  你  卻沒有捨棄
+    Bbm7      Dbm6      Ab   Adim7
+  願一生跟主   奉上給  祢
+  
+  Coda
+    Bbm7      Dbm6     Fm7
+  願一生跟主   奉上給  祢
+  
+  Outro
+  | Fm7 - - Cm/Eb  | Dbadd9 | Abmaj9 | Dbadd9 - Dbm6 - |
+
+`.trim()
+};
